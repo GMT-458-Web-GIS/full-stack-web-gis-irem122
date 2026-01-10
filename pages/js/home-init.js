3f5754e82020c80c9c1b4e5e6b34d007ca383607
irem <irem@192.168.1.21>
2026-01-10 21:00:22 +0300

    fix: remove auto-redirect from home-init.js

--- pages/js/home-init.js
+++ pages/js/home-init.js
@@ -1,23 +1,8 @@
 // Home page initialization
-import { auth } from './firebase-client.js';
-import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js';
 
 // Set localStorage for guest button
 document.getElementById('guestBtn').addEventListener('click', (e) => {
   localStorage.setItem('guest', 'true');
 });
 
-// Redirect logged-in user to map (only if email/password authenticated, not anonymous)
-onAuthStateChanged(auth, (user) => {
-  // Don't redirect if coming from guest logout
-  const wasGuest = localStorage.getItem('guest') === 'true'
-  if (wasGuest) {
-    localStorage.removeItem('guest')
-    return
-  }
-  
-  if (user && !user.isAnonymous) {
-    // Only redirect if user is authenticated with email/password (not anonymous)
-    window.location.href = './map.html';
-  }
-});
+// NO automatic redirect - user must manually choose login method
